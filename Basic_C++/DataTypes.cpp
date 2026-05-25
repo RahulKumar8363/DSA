ff47bf8005ee15da8aca41feafdb64a55c8c3552
RahulKumar8363 <rk8459777@gmail.com>
2026-05-25 11:13:10 +0530

    DataType

---
+++ Basic_C++/DataTypes.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*  1.Integer-Store only the numbers(123)
+    //int  data type
+    int x = 10;
+    cout<<x;
+    //long data type
+    long x = 10;
+    //long long dtata type
+    long long x = 150000; */
+    
+    /* 2. Float,double - Stroe only the floating number (12.666)
+    float x = 2.34;
+    double y = 5;
+    cout << "Value of y: <<y"
+    */ 
+
+    // 3. String and getline
+    /*string s1;
+    string s2;
+    cin >> s1,s2;
+    cout<<s1 <<" " << s2; 
+
+    string str;
+    getline(cin,str);
+    cout<<str;*/
+
+    // 4. Characters data types all the 356  single value can store.
+
+    /*char ch;
+    cin >> ch;
+    cout << ch;*/
+
+
+    return 0;
+
+}
