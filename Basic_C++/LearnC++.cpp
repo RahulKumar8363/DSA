5cb91147bccdd72b435f403039f31e19afb3df30
RahulKumar8363 <rk8459777@gmail.com>
2026-05-25 10:39:14 +0530

    Input/Output

---
+++ Basic_C++/LearnC++.cpp
@@ -0,0 +1,27 @@
+/*#include<iostream>
+int main(){
+    //std::cout<<"HeyStriver!"<<"\n";
+       std::cout<<"HeyStriver!"<< std::endl<< "Hey Rahul"<<"\n";
+    std::cout<<"HeyStriver!";
+    return 0;
+} */
+/*#include<iostream>
+using namespace std;
+int main(){
+    //How to take Input using cin
+    int x , y;
+    cin >> x >> y;
+    cout<<"Value of x:" << x << "\n and y:" << y;
+    return 0;
+
+}*/
+#include<bits/stdc++.h> // incude all the libraries
+using namespace std;
+int main(){
+    //How to take Input using cin
+    int x , y;
+    cin >> x >> y;
+    cout<<"Value of x:" << x << "\n and y:" << y;
+    return 0;
+
+}
