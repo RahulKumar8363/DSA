e913802378d15d1ef04cd424812706fa85d1cf34
RahulKumar8363 <rk8459777@gmail.com>
2026-05-27 07:46:51 +0530

    For While loop

---
+++ Basic_C++/For_Loop.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // for(int i = 10; i > 0; i=i - 1){
+    //     cout<< i << endl;
+    // }
+    // 1. using for loop print even number into 1 to 100
+
+    /*for(int i = 0; i <=100; i++){
+        if(i %2==0){
+            cout<< i << endl ;
+        }
+    }*/
+
+    /* 2. Print Pattern
+    *
+    **
+    ***
+    ****
+    *****
+    */
+//    for(int i =1; i<=5; i++){
+//     for(int j =1; j<= i; j++){
+//          cout<< "*" ; 
+//     }
+//     cout<< endl;
+//    } 
+
+
+    // int i = 1;
+    // while(i <=5){
+    //     cout<<"Rahul" << i << endl;
+    //     i+=1;
+    // }
+    // 1. print 1 to n numbers
+    /*int n, i = 1;
+    cin >> n;
+    while(i <= n){
+        cout<< i << " ";;
+        i++;
+    }*/
+   //resverse this
+   int n, i;
+   cin>> n;
+   i=n;
+   while (i >= 1){
+    cout<< i << " ";
+    i--;
+   }
+     
+    return 0;
+
+}
