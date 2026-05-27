63936ee7009b460e4462071b8125554851d1066d
RahulKumar8363 <rk8459777@gmail.com>
2026-05-27 08:27:57 +0530

    functions

---
+++ Basic_C++/Functions.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Function are set of code which perform something for you
+// Functions are used to modularise the code
+// Function are used to increase readability
+// functions are used to same code multiple times 
+// void - which does not retuen anything
+// return 
+// parameterized
+//non para
+
+// void PrintName(string name) {
+//     cout << "hey " << name << endl;
+// }
+// int main(){
+//     string name;
+//     cin >> name;
+//     PrintName(name);
+//     string name2;
+//     cin >> name2;
+ // PrintName(name2);
+
+// take two numbers and print its sum
+
+// int sum(int num1, int num2){
+//     int num3 = num1 + num2;
+//     return num3;
+// }
+
+// int main(){
+//     int num1, num2;
+//     cin >> num1 >> num2;
+//     int res =sum(num1, num2);
+//     cout<< res;
+//    return 0;    
+// // }
+
+// int maxx(int num1, int num2){
+//     if(num1 >= num2) return num1;
+//     else return num2;
+// }
+// int main(){
+//     int num1, num2;
+//     cin >> num1 >> num2;
+//     int maximum = maxx(num1, num2);
+//     cout << maximun;
+//     return 0;
+// } 
