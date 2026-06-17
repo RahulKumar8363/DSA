a1ce57599d32b861ebf14359041f74d90dcedbee
RahulKumar8363 <rk8459777@gmail.com>
2026-06-17 20:40:33 +0530

    Selection Sort

--- Pattern/Patterns.cpp
+++ Pattern/Patterns.cpp
@@ -102,29 +102,141 @@
 //   *****
 //  *******
 // *********
-# include<bits/stdc++.h>
-using namespace std;
-void print5(int n){
-    for(int i=1;i<=n;i++){
+// # include<bits/stdc++.h>
+// using namespace std;
+// void print5(int n){
+//     for(int i=1;i<=n;i++){
 
-        //space
-        for(int j=1;j <=n-i;j++){
+//         //space
+//         for(int j=1;j <=n-i;j++){
             
-            cout << " " ;
+//             cout << " " ;
+//         }
+//         for(int j=1; j<=2*i-1;j++){
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// int main(){
+//     int t;
+//     cin >> t;
+//     for(int i = 0;i<t;i++){
+//         int n;
+//         cin >> n;
+//         print5(n);
+//     }
+// }
+
+// pattern 6
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// void print6(int n){
+//     for(int i = 1; i <= n; i++){
+
+//         // spaces
+//         for(int j = 1; j <= i - 1; j++){
+//             cout << " ";
+//         }
+
+//         // stars
+//         for(int j = 1; j <= 2*n - (2*i - 1); j++){
+//             cout << "*";
+//         }
+
+//         cout << endl;
+//     }
+// }
+
+// int main(){
+//     int t;
+//     cin >> t;
+
+//     for(int i = 0; i < t; i++){
+//         int n;
+//         cin >> n;
+//         print6(n);
+//     }
+// }
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int largestElement(vector<int> &arr, int n){
+//     int largest = arr[0];
+
+//     for(int i = 0; i < n; i++){
+//         if(arr[i] > largest){
+//             largest = arr[i];
+//         }
+//     }
+//     return largest;
+// }
+
+// int main(){
+//     vector<int> arr = {3, 7, 2, 9, 5};
+//     int n = arr.size();
+
+//     cout << largestElement(arr, n);
+// }
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int largestElement(vector<int> &nums){
+//     int largest = nums[0];
+
+//     for(int i = 0; i < nums.size(); i++){
+//         if(nums[i] > largest){
+//             largest = nums[i];
+//         }
+//     }
+
+//     return largest;
+// }
+
+// int main(){
+//     vector<int> nums = {-4, -3, 0, 1, -8};
+
+//     int ans = largestElement(nums);
+
+//     cout << "Largest element is: " << ans;
+
+//     return 0;
+// }
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int secondLargest(vector<int> &nums){
+    int largest = INT_MIN;
+    int second = INT_MIN;
+
+    for(int i = 0; i < nums.size(); i++){
+
+        if(nums[i] > largest){
+            second = largest;        // पुराना largest → second
+            largest = nums[i];       // नया largest
         }
-        for(int j=1; j<=2*i-1;j++){
-            cout << "*";
+        else if(nums[i] > second && nums[i] != largest){
+            second = nums[i];
         }
-        cout << endl;
     }
+
+    if(second == INT_MIN) return -1;
+
+    return second;
 }
 
 int main(){
-    int t;
-    cin >> t;
-    for(int i = 0;i<t;i++){
-        int n;
-        cin >> n;
-        print5(n);
-    }
+    vector<int> nums = {8, 8, 7, 6, 5};
+
+    cout << "Second largest: " << secondLargest(nums);
 }
---
+++ Sorting/Selection_sort.cpp
@@ -0,0 +1,58 @@
+// Selection Sort with n elements 
+#include<bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+        int minIndex=i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex=j;
+            }
+        }
+        int temp=arr[minIndex];
+        arr[minIndex]=arr[i];
+        arr[i]=temp;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++) cin >> arr[i];
+
+    selection_sort(arr, n);
+
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+}
+
+// With User Input arr[]={2,5,6,1,7,4,8}
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[]={2,5,6,1,7,4,8};
+    int n=7;
+
+    for(int i=0; i<n-1; i++){
+        int minIndex=i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[minIndex]){
+                minIndex=j;
+            }
+        }
+        swap(arr[minIndex],arr[i]);
+    }
+    for(int i=0; i<n; i++){
+    cout << arr[i] <<" ";
+    }
+}
+
+// Space Complexcity = o(1) constant
+// Time Compexcity = o(n^2)
+// Best case = o(n^2)
+// Worst case = o(n^2)
+// Use Case = best for small size
